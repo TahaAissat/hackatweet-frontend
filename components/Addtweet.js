5659742c93c36c8a8e72ae0215157c978589fc03
TahaAissat <tahayacineaissat@gmail.com>
2023-10-05 16:45:32 +0200

    letsgo

--- components/Addtweet.js
+++ components/Addtweet.js
@@ -2,25 +2,28 @@ import styles from '../styles/Home.module.css'
 import { useDispatch,useSelector } from 'react-redux';
 import { defineList} from '../reducers/tweets'
 import { useEffect, useState } from 'react';
+
 function Addtweet(){
+
     const dispatch = useDispatch()
     const [text,setText] = useState('')
     const user = useSelector(state => state.users.value)
-    // console.log('test',user)
-    const hashtagTest = /#[a-z]*/i
-    const hashtag = hashtagTest.test(text)
+    // Isolation des #hashtag
+    const hashtagTest = /#[a-z0-9]+/gi
+    const hashtag = text.match(hashtagTest)
     console.log(hashtag)
+
+
     const handlAddtweet = () =>{
-        fetch('http://localhost:3000/users/add',{
+        fetch('http://localhost:3000/tweets/add',{
       method:'POST',
       headers : {'Content-type' : 'application/json'},
-      body:JSON.stringify({username: user.username, texte: text, firstname: user.firstname,})
+      body:JSON.stringify({username: user.username, texte: text, firstname: user.firstname, hashtag:hashtag,token:user.token})
     })
-
     .then(response => response.json())
     .then(data => {
       console.log(data)
-    //   dispatch(defineList({username:data.username, texte:data.texte, firstname:data.firstname}))
+      setText('')
     })
 
     }
