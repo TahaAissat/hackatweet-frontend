5f5cdaaa3bd4d58f5bb210ddd067159e421dea84
Alexandru <x238785@gmail.com>
2023-10-05 15:20:54 +0200

    frontend

--- components/Addtweet.js
+++ components/Addtweet.js
@@ -1,27 +1,31 @@
 import styles from '../styles/Home.module.css'
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
+import { defineList} from '../reducers/tweets'
+import { useEffect, useState } from 'react';
 function Addtweet(){
     const dispatch = useDispatch()
-    const user = useSelector((state) => state.users.value)
+    const [text,setText] = useState('')
+    const user = useSelector(state => state.users.value)
+    console.log('test',user)
     const handlAddtweet = () =>{
         fetch('http://localhost:3000/users/add',{
       method:'POST',
       headers : {'Content-type' : 'application/json'},
-      body:JSON.stringify({username: user.username,password: user.password})
+      body:JSON.stringify({username: user.username, texte: text, firstname: user.firstname,hashtag: user.hashtag, token: user.token, date: user.date})
     })
 
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      dispatch(addUser({username:data.username, token:data.token}))
+    //   dispatch(defineList({username:data.username, texte:data.texte, firstname:data.firstname}))
     })
-    
+
     }
     return(
     <div className={styles.Addtweet}>
     <label>
      <h3>Home</h3>
-      <textarea onChange={(e) => setUserName(e.target.value)} name="postContent" rows={4} cols={40} />
+      <textarea onChange={(e) => setText(e.target.value)} name="postContent" rows={4} cols={40} />
     </label>
     <button onClick={()=> handlAddtweet()}>tweet</button>
     </div>
--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,13 @@
 import styles from '../styles/Home.module.css';
 import Leftside from '../components/Leftside';
 import LatestTweets from './LatestTweets';
+import Addtweet from './Addtweet';
 
 function Home() {
   return (
     <div className={styles.Home_page}>
         <Leftside/>
+        <Addtweet/>
         <LatestTweets/>
     </div>
   );
